4e47f2863dc7a9ea10d67fee566e0996e7715bf0
juliodiaz <julio@vimbly.com>
2021-06-16 21:30:40 -0500

    Add side-effect (async) in action creator

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { uiActions } from './store/ui-slice';
 import Notifications from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
-let initialStatus = true;
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
@@ -15,51 +16,62 @@ function App() {
   const cart = useSelector(state => state.cart);
   const notification = useSelector(state => state.ui.notification);
 
+  //For side-effect in action creator (Thunk)
   useEffect(() => {
-    const sendCartData = async() => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data...'
-        })
-      )
-
-      const response = await fetch('https://react-http-redux-default-rtdb.firebaseio.com/cart.json',
-      {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      })
-  
-      if(!response.ok){
-        throw new Error('Sending cart data failed.')
-      }
-  
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success',
-          message: 'Cart sent successfully'
-        })
-      )
-    }
-
-    if(initialStatus){
-      initialStatus = false;
+    if(isInitial){
+      isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      console.log(error);
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed'
-        })
-      )
-    })
+    dispatch(sendCartData(cart));
   }, [cart, dispatch])
+
+  // // For side-effect in component
+  // useEffect(() => {
+  //   const sendCartData = async() => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'pending',
+  //         title: 'Sending...',
+  //         message: 'Sending cart data...'
+  //       })
+  //     )
+
+  //     const response = await fetch('https://react-http-redux-default-rtdb.firebaseio.com/cart.json',
+  //     {
+  //       method: 'PUT',
+  //       body: JSON.stringify(cart)
+  //     })
+  
+  //     if(!response.ok){
+  //       throw new Error('Sending cart data failed.')
+  //     }
+  
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'success',
+  //         title: 'Success',
+  //         message: 'Cart sent successfully'
+  //       })
+  //     )
+  //   }
+
+  //   if(isInitial){
+  //     isInitial = false;
+  //     return;
+  //   }
+
+  //   sendCartData().catch((error) => {
+  //     console.log(error);
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'error',
+  //         title: 'Error!',
+  //         message: 'Sending cart data failed'
+  //       })
+  //     )
+  //   })
+  // }, [cart, dispatch])
 
   return (
     <Fragment>
